47bf0bed81ff1db5a487d12f9c37dda2172f2c93
injamamul1564 <146629550+injamamul1564@users.noreply.github.com>
2026-08-24 18:18:35 +0600

    Add theming to App composable
    Wrap App composable in PyHackerTheme for theming.

--- app/src/main/java/com/aistudio/pyhacker/k9v2/MainActivity.kt
+++ app/src/main/java/com/aistudio/pyhacker/k9v2/MainActivity.kt
@@ -3,16 +3,18 @@ package com.aistudio.pyhacker.k9v2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.aistudio.pyhacker.k9v2.ui.theme.PyHackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            PyHackerTheme {
+                App()
+            }
         }
     }
 }
